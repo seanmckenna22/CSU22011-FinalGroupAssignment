59966fa49b1b798f0895d9154ca7091e828f95d8
seanmckenna22 <semckenn@tcd.ie>
2021-04-14 12:40:47 +0100

    Create SearchArrivalTime.java
    Skeleton file create for the java class that will search through the arrival times and return those that satisfy the criteria

---
+++ src/SearchArrivalTime.java
@@ -0,0 +1,14 @@
+public class SearchArrivalTime {
+    /**
+     * This file will search through the arrival times and return those that satisfy
+     * the given criteria
+     *
+     * This will require the bus stop times to be sorted so that they can be searched efficiently
+     */
+
+    public static void main(String[] args)
+    {
+        
+    }
+
+}
