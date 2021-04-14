2c64d7d049ed2b681715fae58bd36aa0e9ff1f4a
seanmckenna22 <semckenn@tcd.ie>
2021-04-14 12:18:37 +0100

    SkeletonFile Created for Bust Stop Search
    Created a skeleton file for the bus stop search java class

--- src/shortestRoutePath.java
+++ src/shortestRoutePath.java
@@ -3,7 +3,7 @@ public class shortestRoutePath {
     /**
      * This is a skeleton file to find the shortest route path, display the route and
      * the associated costs of the route
-     * This uses a floyd Warshall Implimentation
+     * This will use a floyd Warshall Implementation
      */
 
     public static void main(String[] args) {
