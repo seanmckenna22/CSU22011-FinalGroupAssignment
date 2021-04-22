f88668916c2911ebdf0225cfc890ad9441ba4727
ocallagw <82473100+ocallagw@users.noreply.github.com>
2021-04-22 15:17:32 +0100

    Update shortestRoutePath.java

--- src/shortestRoutePath.java
+++ src/shortestRoutePath.java
@@ -10,46 +10,192 @@ public class shortestRoutePath {
 
     
         final static int INF = 99999, V = 4;
-    
-        void floydWarshall(int graph[][])
-        {
-            int dist[][] = new int[V][V];
-            int i, j, k;
 
-            for (i = 0; i < V; i++)
-                for (j = 0; j < V; j++)
-                    dist[i][j] = graph[i][j];
+    public class DijkstraSP {
+        private double[] distTo;          // distTo[v] = distance  of shortest s->v path
+        private DirectedEdge[] edgeTo;    // edgeTo[v] = last edge on shortest s->v path
+        private IndexMinPQ<Double> pq;    // priority queue of vertices
 
-            for (k = 0; k < V; k++)
-            {
-                for (i = 0; i < V; i++)
-                {
-                    for (j = 0; j < V; j++)
-                    {
-                        if (dist[i][k] + dist[k][j] < dist[i][j])
-                            dist[i][j] = dist[i][k] + dist[k][j];
+        /**
+         * Computes a shortest-paths tree from the source vertex {@code s} to every other
+         * vertex in the edge-weighted digraph {@code G}.
+         *
+         * @param  G the edge-weighted digraph
+         * @param  s the source vertex
+         * @throws IllegalArgumentException if an edge weight is negative
+         * @throws IllegalArgumentException unless {@code 0 <= s < V}
+         */
+        public DijkstraSP(EdgeWeightedDigraph G, int s) {
+            for (DirectedEdge e : G.edges()) {
+                if (e.weight() < 0)
+                    throw new IllegalArgumentException("edge " + e + " has negative weight");
+            }
+
+            distTo = new double[G.V()];
+            edgeTo = new DirectedEdge[G.V()];
+
+            validateVertex(s);
+
+            for (int v = 0; v < G.V(); v++)
+                distTo[v] = Double.POSITIVE_INFINITY;
+            distTo[s] = 0.0;
+
+            // relax vertices in order of distance from s
+            pq = new IndexMinPQ<Double>(G.V());
+            pq.insert(s, distTo[s]);
+            while (!pq.isEmpty()) {
+                int v = pq.delMin();
+                for (DirectedEdge e : G.adj(v))
+                    relax(e);
+            }
+
+            // check optimality conditions
+            assert check(G, s);
+        }
+
+        // relax edge e and update pq if changed
+        private void relax(DirectedEdge e) {
+            int v = e.from(), w = e.to();
+            if (distTo[w] > distTo[v] + e.weight()) {
+                distTo[w] = distTo[v] + e.weight();
+                edgeTo[w] = e;
+                if (pq.contains(w)) pq.decreaseKey(w, distTo[w]);
+                else                pq.insert(w, distTo[w]);
+            }
+        }
+
+        /**
+         * Returns the length of a shortest path from the source vertex {@code s} to vertex {@code v}.
+         * @param  v the destination vertex
+         * @return the length of a shortest path from the source vertex {@code s} to vertex {@code v};
+         *         {@code Double.POSITIVE_INFINITY} if no such path
+         * @throws IllegalArgumentException unless {@code 0 <= v < V}
+         */
+        public double distTo(int v) {
+            validateVertex(v);
+            return distTo[v];
+        }
+
+        /**
+         * Returns true if there is a path from the source vertex {@code s} to vertex {@code v}.
+         *
+         * @param  v the destination vertex
+         * @return {@code true} if there is a path from the source vertex
+         *         {@code s} to vertex {@code v}; {@code false} otherwise
+         * @throws IllegalArgumentException unless {@code 0 <= v < V}
+         */
+        public boolean hasPathTo(int v) {
+            validateVertex(v);
+            return distTo[v] < Double.POSITIVE_INFINITY;
+        }
+
+        /**
+         * Returns a shortest path from the source vertex {@code s} to vertex {@code v}.
+         *
+         * @param  v the destination vertex
+         * @return a shortest path from the source vertex {@code s} to vertex {@code v}
+         *         as an iterable of edges, and {@code null} if no such path
+         * @throws IllegalArgumentException unless {@code 0 <= v < V}
+         */
+        public Iterable<DirectedEdge> pathTo(int v) {
+            validateVertex(v);
+            if (!hasPathTo(v)) return null;
+            Stack<DirectedEdge> path = new Stack<DirectedEdge>();
+            for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
+                path.push(e);
+            }
+            return path;
+        }
+
+
+        // check optimality conditions:
+        // (i) for all edges e:            distTo[e.to()] <= distTo[e.from()] + e.weight()
+        // (ii) for all edge e on the SPT: distTo[e.to()] == distTo[e.from()] + e.weight()
+        private boolean check(EdgeWeightedDigraph G, int s) {
+
+            // check that edge weights are non-negative
+            for (DirectedEdge e : G.edges()) {
+                if (e.weight() < 0) {
+                    System.err.println("negative edge weight detected");
+                    return false;
+                }
+            }
+
+            // check that distTo[v] and edgeTo[v] are consistent
+            if (distTo[s] != 0.0 || edgeTo[s] != null) {
+                System.err.println("distTo[s] and edgeTo[s] inconsistent");
+                return false;
+            }
+            for (int v = 0; v < G.V(); v++) {
+                if (v == s) continue;
+                if (edgeTo[v] == null && distTo[v] != Double.POSITIVE_INFINITY) {
+                    System.err.println("distTo[] and edgeTo[] inconsistent");
+                    return false;
+                }
+            }
+
+            // check that all edges e = v->w satisfy distTo[w] <= distTo[v] + e.weight()
+            for (int v = 0; v < G.V(); v++) {
+                for (DirectedEdge e : G.adj(v)) {
+                    int w = e.to();
+                    if (distTo[v] + e.weight() < distTo[w]) {
+                        System.err.println("edge " + e + " not relaxed");
+                        return false;
                     }
                 }
             }
-            printSolution(dist);
-        }
-    
-        void printSolution(int dist[][])
-        {
-            System.out.println("The following matrix shows the shortest "+
-                    "distances between every pair of vertices");
-            for (int i=0; i<V; ++i)
-            {
-                for (int j=0; j<V; ++j)
-                {
-                    if (dist[i][j]==INF)
-                        System.out.print("INF ");
-                    else
-                        System.out.print(dist[i][j]+"   ");
+
+            // check that all edges e = v->w on SPT satisfy distTo[w] == distTo[v] + e.weight()
+            for (int w = 0; w < G.V(); w++) {
+                if (edgeTo[w] == null) continue;
+                DirectedEdge e = edgeTo[w];
+                int v = e.from();
+                if (w != e.to()) return false;
+                if (distTo[v] + e.weight() != distTo[w]) {
+                    System.err.println("edge " + e + " on shortest path not tight");
+                    return false;
                 }
-                System.out.println();
+            }
+            return true;
+        }
+
+        // throw an IllegalArgumentException unless {@code 0 <= v < V}
+        private void validateVertex(int v) {
+            int V = distTo.length;
+            if (v < 0 || v >= V)
+                throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+        }
+
+        /**
+         * Unit tests the {@code DijkstraSP} data type.
+         *
+         * @param args the command-line arguments
+         */
+        public static void main(String[] args) {
+            In in = new In(args[0]);
+            EdgeWeightedDigraph G = new EdgeWeightedDigraph(in);
+            int s = Integer.parseInt(args[1]);
+
+            // compute shortest paths
+            DijkstraSP sp = new DijkstraSP(G, s);
+
+
+            // print shortest path
+            for (int t = 0; t < G.V(); t++) {
+                if (sp.hasPathTo(t)) {
+                    StdOut.printf("%d to %d (%.2f)  ", s, t, sp.distTo(t));
+                    for (DirectedEdge e : sp.pathTo(t)) {
+                        StdOut.print(e + "   ");
+                    }
+                    StdOut.println();
+                }
+                else {
+                    StdOut.printf("%d to %d         no path\n", s, t);
+                }
             }
         }
+
+    }
 
         private void fileReader(){
 
@@ -64,11 +210,7 @@ public class shortestRoutePath {
         private int timeRequired(){
 
         }
-
-        public static void main (String[] args)
-        {
-            
-        }
+        
     }
     
     
